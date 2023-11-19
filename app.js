7a1d872ba05625ef983959609061840d70b48f52
Oleh <dremliuhaoo.gmail.com>
2023-11-19 23:44:22 +0200

    second

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const http = require("http")
 
 http.createServer(function(request, response){
-    console.log("Url: " + request.url);
-    console.log("Тип запроса: " + request.method);
-    console.log("User-Agent: " + request.headers["user-agent"]);
-    console.log("Все заголовки");
-    console.log(request.headers);
+    
+    response.setHeader("Content-Type", "text; charset=utf-8;");
 
+    if(request.url === "/home" || request.url === "/"){
+        response.write("Work");
+    }
 
     response.end("NodeJs");
 

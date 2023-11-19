d808a24595def599dcd4bb80350ac19f2a17906d
Oleh <dremliuhaoo.gmail.com>
2023-11-19 23:35:16 +0200

    first try

---
+++ app.js
@@ -0,0 +1,16 @@
+const http = require("http")
+
+http.createServer(function(request, response){
+    console.log("Url: " + request.url);
+    console.log("Тип запроса: " + request.method);
+    console.log("User-Agent: " + request.headers["user-agent"]);
+    console.log("Все заголовки");
+    console.log(request.headers);
+
+
+    response.end("NodeJs");
+
+
+}).listen(3000, "127.0.0.1", function(){
+    console.log("Server start listening");
+});
